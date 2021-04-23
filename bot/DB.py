e659d85451d5ebfa1e69426d6ac08ad8481d80f1
Kaloyan <kaloyan.vylchev@gmail.com>
2021-04-23 10:32:55 +0100

    Fuzzymatch game name + pull data for it
    + Checking similar names in the DB. Finds the column with closest match to the name given
    + Get's the whole row of data for the related game

--- bot/DB.py
+++ bot/DB.py
@@ -4,6 +4,9 @@ from datetime import datetime
 import pandas as pd
 import csv
 import numpy as np
+from fuzzywuzzy import fuzz
+import re
+
 
 
 conn = psycopg2.connect(host='localhost', port=5433, user='postgres', password='postgres')
@@ -73,11 +76,43 @@ def add_game_codes_to_db():
     conn.close()
 
 
-def get_similar_names_from_db(name):
+def get_similar_names_from_db(game_name):
     cur = conn.cursor()
-    name0_query = "SELECT * FROM bgg_data WHERE name0_alt IS NOT NULL AND name0_alt ILIKE '{}'".format(name)
-    cur.execute(name0_query)
-    name0 = cur.fetchall()
+    all_names_query = "SELECT name0, name1, name2, name3, name4, name5, name6, name7, name8, name9 FROM bgg_data"
+    cur.execute(all_names_query)
+    all_names = cur.fetchall()
+    for row in all_names:
+        match_ratio = 0
+        for name in enumerate(row):
+            if fuzz.ratio(name[1].lower(), game_name.lower()) > match_ratio:
+                match_ratio = fuzz.ratio(name[1].lower(), game_name.lower())
+                print("Comparing {} with '{}'. Similarity ratio is : {}".format(name[1].lower(),game_name.lower(), match_ratio))
+                closest_name = name
+        if match_ratio >= 70:
+            print(closest_name)
+            break
+
+    cur.close()       
+    return closest_name   
     
 
-get_similar_names_from_db("gloomhaven")
+def get_data_from_db_based_on_name(game_name):
+    # Get all relevant information for particular game, given it's name
+    # Returns: data row for game with similar name.
+    selected_game = get_similar_names_from_db(game_name)
+    cur = conn.cursor()
+    row_data_query = "SELECT average,usersrated,minplayers,maxplayers,playingtime,minplaytime,maxplaytime, \
+        yearpublished,boardgamerank,designer0,artist0,artist1,subdomain0,category0,category1,category2, \
+        honor0,honor1,expansion0,expansion1,expansion2,description,thumbnail\
+                  FROM bgg_data WHERE name{} = '{}'".format(selected_game[0],selected_game[1])
+    cur.execute(row_data_query)
+    game_data = cur.fetchall()
+    print(game_data[0][21])
+    # clean_description = re.sub('<[^<]+?>', '', game_data[0][21])
+    print("yes")
+
+    cur.close()
+
+    return game_data
+
+get_data_from_db_based_on_name("gloomhaven")
